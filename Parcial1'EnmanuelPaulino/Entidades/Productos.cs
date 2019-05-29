6fe4252fe7223605402caf1e6fca570e6b7de2d8
Enmanuel Paulino Seg <enmanuel_paulino@ucne.edu.do>
2019-05-29 15:27:54 -0400

    Clase de Entidades

---
+++ Parcial1'EnmanuelPaulino/Entidades/Productos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_EnmanuelPaulino.Entidades
+{
+   public class Productos
+    {
+        [Key]
+        public  int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Existencia { get; set; }
+        public decimal Costo { get; set; }
+        public decimal ValorInventario { get; set; }
+
+        public Productos()
+        {
+            ProductoId = 0;
+            Descripcion = string.Empty;
+            Existencia = 0;
+            Costo = 0;
+            ValorInventario = 0;
+        }
+    }
+    
+}
