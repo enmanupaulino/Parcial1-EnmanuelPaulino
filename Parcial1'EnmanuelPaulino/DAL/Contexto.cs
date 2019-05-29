8193105585c234d6b6d594ce0dbb84acdadc0db7
Enmanuel Paulino Seg <enmanuel_paulino@ucne.edu.do>
2019-05-29 15:37:15 -0400

    Contexto Creado

---
+++ Parcial1'EnmanuelPaulino/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Parcial1_EnmanuelPaulino.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1_EnmanuelPaulino.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Productos> productos { get; set; }
+        public Contexto() : base("ConStr") { }
+
+    }
+}
